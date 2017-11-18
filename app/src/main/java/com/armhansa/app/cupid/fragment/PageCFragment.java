fd506b22ac65e6ce6b04921c7da7c6d84e1698e3
Arm Hansa <armhansa@gmail.com>
2017-11-19 02:00:31 +0700

    Create Project

---
+++ app/src/main/java/com/armhansa/app/cupid/fragment/PageCFragment.java
@@ -0,0 +1,26 @@
+package com.armhansa.app.cupid.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.armhansa.app.cupid.R;
+
+public class PageCFragment extends Fragment {
+
+    public PageCFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_page_c, container, false);
+    }
+
+}
